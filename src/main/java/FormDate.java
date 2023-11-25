e7313297f7240dfc5d3d2862c4fd7d3171a1472a
Sergey Medvedev <msergejev@mail.ru>
2023-11-25 11:15:26 +0300

    Initial commit

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,6 @@
+public class FormDate {
+    public int day; // день
+    public int month; // месяц
+    public int year; // год
+
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле для паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для телефона
+    public String surname; // поле для фамилии
+    public boolean subscription;
+    public FormDate birthday;
+
+}
